650f22315e07469a7298468f25832d5eb058bc98
ximemunoz <92742110+ximemunoz@users.noreply.github.com>
2022-10-25 21:45:14 -0500

    final

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
 
 app.use('/articles', articleRouter)
-
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: Date.now(),
-        description: 'Test description'
-    }] 
-    res.render('index', {articles: articles})
-})
 
 app.listen(5000)
